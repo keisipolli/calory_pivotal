d3405c7467f9a60831d05ae0e47ed469f6085e09
Keisi Polli <keisi.polli@khk.ee>
2022-01-25 11:36:26 +0200

    As user I can see meal names and their calories [Delivers #180988478]

---
+++ app.js
@@ -0,0 +1,154 @@
+// Storage Controller
+const StorageCtrl = (function(){
+    // public methods
+    return {
+        getItemsFromStorage: function(){
+            let items;
+            if(localStorage.getItem('items') === null){
+                items = [];
+            } else {
+                items = JSON.parse(localStorage.getItem('items'));
+            }
+            return items;
+        }
+    }
+})();
+
+// Item Controller
+const ItemCtrl = (function(){
+    // Item Constructor
+    const Item = function(id, name, calories){
+        this.id = id
+        this.name = name
+        this.calories = calories
+    }
+
+    // Data Structure
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 1, name: 'Cookie', calories: 400},
+            {id: 2, name: 'Eggs', calories: 300}
+        ],
+        total: 0,
+        currentItem: null
+    }
+
+    return {
+        getItems: function(){
+            return data.items
+        },
+        addItem: function(name, calories){
+            let ID;
+            // Create ID
+            if(data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1
+            } else {
+                ID = 0
+            }
+            // calories to number
+            calories = parseInt(calories);
+            // create new item
+            newItem = new Item(ID, name, calories);
+            // add to items array
+            data.items.push(newItem);
+            // return new item
+            return newItem
+        },
+        logData: function(){
+            return data
+        }
+    }
+})();
+
+
+// UI Controller
+const UICtrl = (function(){
+    // UI selectors
+    const UISelectors = {
+        itemList: '#item-list',
+        listItems: '#item-list li'
+    }
+    return {
+        populateItemList: function(items){
+            // create html content
+            let html = '';
+
+            // parse data and create list items html
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item-${item.id}">
+        		<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        		<a href="#" class="secondary-content">
+          			<i class="edit-item fa fa-pencil"></i>
+        		</a>
+      			</li>`;
+            });
+
+            // insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getSelectors: function(){
+            return UISelectors;
+        },
+        addListItem: function(item){
+            // create li element
+            const li = document.createElement('li');
+            // add class
+            li.className = 'collection-item';
+            // add ID
+            li.id = `item-${item.id}`;
+            // add HTML
+            li.innerHTML = `<strong>${item.name}: </strong> 
+      			<em>${item.calories} Calories</em>
+		    	<a href="#" class="secondary-content">
+		    		<i class="edit-item fa fa-pencil"></i>
+		    	</a>`;
+            // insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+        }
+    }
+})();
+
+// App Controller
+const App = (function(ItemCtrl, StorageCtrl, UICtrl){
+    // Load event listeners
+    const loadEventListeners = function(){
+        // hide update button
+        UICtrl.clearEditState();
+        // get UI selectors
+        const UISelectors = UICtrl.getSelectors();
+        // add document reload event
+        document.addEventListener('DOMContentLoaded', getItemsFromStorage)
+    }
+
+    // get items from storage
+    const getItemsFromStorage = function(){
+        // get items from storage
+        const items = StorageCtrl.getItemsFromStorage()
+        //console.log(items)
+        // set storage items to ItemCtrl data items
+        items.forEach(function(item){
+            ItemCtrl.addItem(item['name'], item['calories'])
+        })
+        // get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        // add total calories to UI
+        UICtrl.showTotalCalories(totalCalories);
+        // populate items list
+        UICtrl.populateItemList(items)
+    }
+    return {
+        init: function(){
+            console.log('Initializing App')
+            // fetch items from data structure
+            const items = ItemCtrl.getItems()
+            // populate items list
+            UICtrl.populateItemList(items)
+            // load event listeners
+            loadEventListeners();
+        }
+    }
+})(ItemCtrl, StorageCtrl, UICtrl);
+
+// Initialize App
+App.init()
